2eca5c7b11dcb14d4c4375ce2b3afa0e7692f2dd
wolframm <arne.wolframm@pm.me>
2024-05-19 11:23:15 +0300

    feat: copying the right pubspec_client

--- preCommit.sh
+++ preCommit.sh
@@ -4,7 +4,7 @@ cd "$DIR"/client || exit; dart run build_runner build &
 cd "$DIR"/cli || exit; dart run build_runner build
 wait
 
-mv lib/src/pubspec.yaml.g.dart "$DIR"/cli/lib/src/pubspec_client.yaml.g.dart
+cd "$DIR"/client || exit; mv lib/src/pubspec.yaml.g.dart "$DIR"/cli/lib/src/pubspec_client.yaml.g.dart
 
 git add .
 git commit -m 'updated versions'
