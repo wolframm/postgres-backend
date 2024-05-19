c2a41baf628b8a7379f82e82db31891a82711071
wolframm <arne.wolframm@pm.me>
2024-05-19 11:19:09 +0300

    feat: added pubspec generator to client lib

--- preCommit.sh
+++ preCommit.sh
@@ -1,10 +1,10 @@
 DIR=$(pwd)
 
-#cd "$DIR"/server || exit; dart run build_runner build &
-#cd "$DIR"/client || exit; dart run build_runner build &
-#cd "$DIR"/converter || exit; dart run build_runner build &
+cd "$DIR"/client || exit; dart run build_runner build &
 cd "$DIR"/cli || exit; dart run build_runner build
-#cd "$DIR"/dev || exit; dart run build_runner build
 wait
+
+mv lib/src/pubspec.yaml.g.dart "$DIR"/cli/lib/src/pubspec_client.yaml.g.dart
+
 git add .
 git commit -m 'updated versions'
