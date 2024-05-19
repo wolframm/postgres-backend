5637a30d51f88d2b2e9ad9f904dbb2cc33a73ab6
wolframm <arne.wolframm@pm.me>
2024-05-19 11:34:08 +0300

    bug: copying pubspec_client

--- preCommit.sh
+++ preCommit.sh
@@ -4,5 +4,7 @@ cd "$DIR"/client || exit; dart run build_runner build &
 cd "$DIR"/cli || exit; dart run build_runner build
 wait
 
+mv "$DIR"/client/lib/src/pubspec.yaml.g.dart "$DIR"/cli/lib/src/pubspec_client.yaml.g.dart
+
 git add .
 git commit -m 'updated versions'
