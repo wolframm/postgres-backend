f725407847aaab1f5a693356382c8f6b968ad93e
wolframm <arne.wolframm@pm.me>
2024-05-19 11:29:01 +0300

    feat: again

--- preCommit.sh
+++ preCommit.sh
@@ -4,7 +4,5 @@ cd "$DIR"/client || exit; dart run build_runner build &
 cd "$DIR"/cli || exit; dart run build_runner build
 wait
 
-cd "$DIR"/client || exit; mv lib/src/pubspec.yaml.g.dart "$DIR"/cli/lib/src/pubspec_client.yaml.g.dart
-
 git add .
 git commit -m 'updated versions'
