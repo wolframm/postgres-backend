016c851ffd77577bac771e846fac2f373c1fc9ae
wolframm <arne.wolframm@pm.me>
2024-05-26 08:17:19 +0300

    feat: added pubspec gen to server lib

--- preCommit.sh
+++ preCommit.sh
@@ -1,10 +1,12 @@
 DIR=$(pwd)
 
 cd "$DIR"/client || exit; dart run build_runner build &
+cd "$DIR"/server || exit; dart run build_runner build &
 cd "$DIR"/cli || exit; dart run build_runner build
 wait
 
 mv "$DIR"/client/lib/src/pubspec.yaml.g.dart "$DIR"/cli/lib/src/pubspec_client.yaml.g.dart
+mv "$DIR"/server/lib/src/pubspec.yaml.g.dart "$DIR"/cli/lib/src/pubspec_server.yaml.g.dart
 
 git add .
 git commit -m 'updated versions'
